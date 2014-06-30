8c7ce08e7d20c6f9b428ac6a79b09eadd9c5bdaa
Ryan <cumt.xiaochi@gmail.com>
2014-06-30 16:05:17 +0800

    sql

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,9 +1,109 @@
-CREATE TABLE tbl_user (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(128) NOT NULL,
-    password VARCHAR(128) NOT NULL,
-    email VARCHAR(128) NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 4.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: 2014-06-30 16:04:39
+-- 服务器版本： 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ppt_sin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ji_comment`
+--
+
+CREATE TABLE IF NOT EXISTS `ji_comment` (
+`id` int(10) unsigned NOT NULL,
+  `ppt_id` int(10) unsigned NOT NULL,
+  `nick_name` varchar(20) NOT NULL COMMENT '昵称',
+  `text_` text NOT NULL COMMENT '文字',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='评论' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ji_ppt`
+--
+
+CREATE TABLE IF NOT EXISTS `ji_ppt` (
+`id` int(10) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT '名称',
+  `url` varchar(255) NOT NULL COMMENT '网址',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='ppt' AUTO_INCREMENT=50 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ji_user`
+--
+
+CREATE TABLE IF NOT EXISTS `ji_user` (
+`id` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ji_comment`
+--
+ALTER TABLE `ji_comment`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ji_ppt`
+--
+ALTER TABLE `ji_ppt`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ji_user`
+--
+ALTER TABLE `ji_user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ji_comment`
+--
+ALTER TABLE `ji_comment`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `ji_ppt`
+--
+ALTER TABLE `ji_ppt`
+MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=50;
+--
+-- AUTO_INCREMENT for table `ji_user`
+--
+ALTER TABLE `ji_user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 INSERT INTO tbl_user (username, password, email) VALUES ('test1', 'pass1', 'test1@example.com');
 INSERT INTO tbl_user (username, password, email) VALUES ('test2', 'pass2', 'test2@example.com');
