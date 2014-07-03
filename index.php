3b39b3ff464d48a3c5bf4262f89fbeeb0105bf17
Ryan <cumt.xiaochi@gmail.com>
2014-07-03 16:51:42 +0800

    ppt 上传

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 // remove the following lines when in production mode
 defined('DEBUG') or define('DEBUG',true);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/protected/components/lib.php';
 
 $config = require (__DIR__.'/protected/config/main.php');
 if (isset($_SERVER['HTTP_APPNAME'])) {
@@ -23,15 +24,38 @@ $klein = new \Klein\Klein();
 $klein->respond(function () {
 });
 
-$klein->respond('GET', '/', function () {
-    ORM::for_table('');
-    return 'Hello World!';
+$klein->respond('GET', '/', function () use ($config) {
+    $list = ORM::for_table('ppt')
+        ->order_by_desc('id')
+        ->limit(100)
+        ->find_many();
+    include __DIR__.'/protected/views/ppt/index.phtml';
 });
 $klein->respond('GET', '/slide/[:id]', function ($request) {
     return 'Hello ' . $request->name;
 });
-$klein->respond('POST', '/slide/upload', function ($request) {
-    return 'Hello ' . $request->name;
+$klein->respond('POST', '/upload', function ($request) use ($config) {
+    $ppt = ORM::for_table('ppt')->create();
+    $ppt->name = $request->name;
+    if (!isset($_FILES['url']['name'])) {
+        plog('no $_FILES[url]', 'LEVEL_ERROR');
+        die('请选择文件');
+    }
+    $error = $_FILES['url']['error'];
+    if ($error) {
+        plog('upload error '.$error, 'error');
+        header('Location:/');
+        exit;
+    }
+    $ppt->url = upload($config['params']['upload_root'], $_FILES['url']['tmp_name']);
+    $ppt->created = date('Y-m-d H:i:s');
+    $ppt->save();
+    header('Location:/');
+    exit;
+});
+
+$klein->respond('GET', '/upload', function ($request) {
+    include __DIR__.'/protected/views/ppt/create.phtml';
 });
 
 $klein->respond('GET', '/mem/', function ($request) {
